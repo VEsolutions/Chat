ca008d81f1de7e32960b6bf2aa9ec09184d194b7
Björn Englesson <engles@kth.se>
2015-07-25 17:47:40 +0200

    Changed port

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express                 = require('express'),
     http                    = require('http').Server(app),
     io                      = require('socket.io')(http);
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use('/css', express.static(__dirname + '/client/css'));
 app.use('/js', express.static(__dirname + '/client/js'));
 
@@ -20,6 +22,6 @@ io.on('connection', function(socket){
   	});
 });
 
-http.listen(3000, function(){
-	console.log('listening on *:3000');
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
